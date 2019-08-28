e65359349e8cf95a02b3b94f84971bd268418d27
user <user@LENOVO>
2019-08-28 14:45:29 +0530

    Initial

---
+++ TMS1/TMS1/Controllers/RTOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMS1.Controllers
+{
+    public class RTOController : Controller
+    {
+        // GET: RTO
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TMS1/TMS1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMS1.Startup))]
+namespace TMS1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
